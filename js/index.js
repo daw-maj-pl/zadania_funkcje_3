d9e68b5c7c3ecbe0734e34ca5708b36a0ab38f3f
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2020-11-08 15:22:48 +0100

    refaktoring

--- js/index.js
+++ js/index.js
@@ -1,84 +1,64 @@
 // Exercise 7.
 document.querySelector('.to-factorial').addEventListener('input', e => {
   const giveMeFactorial = num => {
+    let result;
     if (num < 0) {
-      return 'BŁĄD! Nie można podawać liczb ujemnych!'
-    }
+      result = 'BŁĄD! Nie można podawać liczb ujemnych!'
+    } else if (num === 0 || num === 1) {
+      result = 1;
+    } else {
+      result = giveMeFactorial(num-1)*num; // tym razem postanowiłem użyć rekurencji
+    }  
 
-    if (num === 0 || num === 1) {
-      return 1;
-    }
-
-    for (let i = num - 1; i >= 1; i--) {
-      num *= i;
-    }
-
-    return num;
-  } 
+    return result;
+  };
 
   document.querySelector('.log-factorial').textContent = giveMeFactorial(parseInt(e.target.value));
 });
 
 // Exercise 8.
-const inkLevels = obj => Math.min(obj.cyan, obj.magenta, obj.yellow);
-
-document.querySelector('.printer-1').addEventListener('click', e => {
-  const printer1 = {
+document.querySelector('.printer').addEventListener('click', e => {
+  const inkLevels = obj => Math.min(obj.cyan, obj.magenta, obj.yellow);
+  const printer = {
     cyan: 32,
     magenta: 21,
     yellow: 11
   };
-
-  document.querySelector('.log-levels').textContent = inkLevels(printer1);
-});
-
-document.querySelector('.printer-2').addEventListener('click', e => {
-  const printer2 = {
-    cyan: 234,
-    magenta: 345,
-    yellow: 753
-  };
-
-  document.querySelector('.log-levels').textContent = inkLevels(printer2);
-});
-
-document.querySelector('.printer-3').addEventListener('click', e => {
-  const printer3 = {
-    cyan: 0,
-    magenta: 600,
-    yellow: 600
-  };
-
-  document.querySelector('.log-levels').textContent = inkLevels(printer3);
+  
+  document.querySelector('.log-levels').textContent = inkLevels(printer);
 });
 
 // Exercise 9.
 document.querySelector('.pH').addEventListener('input', e => {
   const pHName = val => {
+    let result;
     switch (true) {
       case (val >= 0 && val < 7):
-        return 'kwaśny';
+        result = 'kwaśny';
+        break;
       case (val === 7):
-        return 'obojętny';
+        result = 'obojętny';
+        break;
       case (val > 7 && val <= 14):
-        return 'zasadowy';
+        result = 'zasadowy';
+        break;
       default:
-        return 'nieprawidłowy';
+        result = 'nieprawidłowy';
     }
+
+    return result;
   };
 
   const giveMePHName = val => {
-    if (!val && val !== 0) {
-      return null;
-    }
+    let result;
+    if (!val && val !== 0)
+      result = null;
+    else if (pHName(val) === 'nieprawidłowy')
+      result = `BŁĄD! Podana wartość jest niepoprawna. Proszę wybrać wartość od 1 do 14.`;
+    else
+      result = `Odczyn ${pHName(val)}.`;
 
-    const phName = pHName(val);
-    
-    if (phName === 'nieprawidłowy') {      
-      return `BŁĄD! Podana wartość jest niepoprawna. Proszę wybrać wartość od 1 do 14.`;
-    }
-
-    return `Odczyn ${phName}.`;
+    return result;
   } 
 
   document.querySelector('.log-pH-text').textContent = giveMePHName(parseInt(e.target.value));
